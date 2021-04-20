e902d6deff35e70c1960e7652e5b9da1a9cb853e
Luis Sanchez <ingcomluis@gmail.com>
2021-04-19 20:55:26 -0500

    Se agrega tecnologia owl y se avanza on una fila más casos

---
+++ webpsicologia/js/center.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        }
+    }
+})
+
+
