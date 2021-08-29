9afe8f9544a6b77b6b0ac3e39c3182b07498fc74
Luis Sanchez <ingcomluis@gmail.com>
2021-08-28 19:13:36 -0500

    empiezas proyecto webgym

---
+++ webgym/js/autoplay.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
